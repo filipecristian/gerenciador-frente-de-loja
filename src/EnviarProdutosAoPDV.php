081db276ce7a5d9ac0c27033dfa1dcb18db316f4
filipeoliveira <filipe.oliveira@picpay.com>
2021-02-03 02:48:31 -0300

    Criada classe para envio de produtos ao PDV.

--- src/EnviarProdutosAoPDV.php
+++ src/EnviarProdutosAoPDV.php
@@ -2,7 +2,29 @@
 
 namespace GerenciadorFrenteDeLoja;
 
-class EnviarProdutoAoPDV
+use InvalidArgumentException;
+
+class EnviarProdutosAoPDV
 {
-    protected
+    protected array $listaProdutos = [];
+
+    /**
+     * @param Produto $produto
+     */
+    public function adicionar(Produto $produto)
+    {
+        array_push($this->listaProdutos, $produto);
+    }
+
+    /**
+     * @return bool
+     */
+    public function enviar() : bool
+    {
+        if (!$this->listaProdutos) {
+            throw new InvalidArgumentException('Não á produtos a serem enviados.');
+        }
+        // Envia para a fila
+        return true;
+    }
 }
